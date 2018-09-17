2c1d7277a8d80203493e61fff86b704def79a9db
Rreuben <jreuben07@gmail.com>
2018-09-17 11:50:58 +0300

    Added User, Role, Blog, and Comment classes

--- app/models.py
+++ app/models.py
@@ -8,22 +8,22 @@ from . import DB, LOGIN_MANAGER
 
 @LOGIN_MANAGER.user_loader
 def load_user(user_id):
-return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 class User(UserMixin, DB.Model):
+
     __tablename__ = 'users'
 
     id = DB.Column(DB.Integer, primary_key=True)
-    username = DB.Column(DB.String(255), unique=True, nullable=False)
-    email = DB.Column(DB.String(255), unique=True, index=True, nullable=False)
+    username = DB.Column(DB.String(255))
+    email = DB.Column(DB.String(255), unique=True)
+    password_hash = DB.Column(DB.String(255))
+    bio = DB.Column(DB.String(255))
+    profile_pic_path = DB.Column(DB.String())
 
-    image_file = DB.Column(DB.String(70), nullable=False, default='default.jpg')
+    role_id = DB.Column(DB.Integer, DB.ForeignKey('roles.id'))
+    blogs = DB.relationship('Post', backref='user', lazy="dynamic")
     comments = DB.relationship('Comments', backref='user', lazy='dynamic')
-
-    def __repr__(self):
-        return f'User {self.username}'
-
-    pass_secure = DB.Column(DB.String(255))
 
     @property
     def password(self):
@@ -31,11 +31,52 @@ class User(UserMixin, DB.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        return check_password_hash(self.pass_secure, password)
+        return check_password_hash(self.password_hash, password)
 
-    @login_manager.user_loader
-    def load_user(user_id):
-return User.query.get(int(user_id))
+    def __repr__(self):
+        return f'{self.username}'
+
+
+class Role(DB.Model):
+
+    __tablename__ = 'roles'
+
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(255), index=True)
+
+    users = DB.relationship('User', backref='role', lazy='dynamic')
+
+
+class Blog(DB.Model):
+
+    __tablename__ = 'blogs'
+
+    id = DB.Column(DB.Integer, primary_key=True)
+    title = DB.Column(DB.String(255), index=True)
+    content = DB.Column(DB.String(), index=True)
+    time = DB.Column(DB.DateTime, default=datetime.utcnow)
+
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('users.id'))
+    comments = DB.relationship('Comment', backref='blog', lazy='dynamic')
+
+    def save_blog(self):
+        DB.session.add(self)
+        DB.session.commit()
+
+
+class Comment(DB.Model):
+
+    __tablename__ = 'comments'
+
+    id = DB.Column(DB.Integer, primary_key=True)
+    body = DB.Column(DB.String(250))
+
+    post_id = DB.Column(DB.Integer, DB.ForeignKey("posts.id"))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey("users.id"))
+
+    def save_comments(self):
+        DB.session.add(self)
+        DB.session.commit()
