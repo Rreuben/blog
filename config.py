f582d8363898d7a63e033b1296b21611ed75e57f
Rreuben <jreuben07@gmail.com>
2018-09-17 09:39:44 +0300

    Added configurations classes

---
+++ config.py
@@ -0,0 +1,44 @@
+'''Configuration classes module'''
+
+import os
+
+
+class Config:
+
+    '''
+    General configuration parent class
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:password@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdConfig(Config):
+
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    pass
+
+
+class DevConfig(Config):
+
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+CONFIG_OPTIONS = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
